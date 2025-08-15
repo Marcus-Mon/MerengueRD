deae0cffbffb8f5f8bdcf74e45d500732a33cb2e
Marcus-Mon <marcusrafaelmontilla@gmail.com>
2025-08-15 15:45:27 -0400

    Añadiendo el datacontext y el EF

---
+++ MerengueRD/MerengueRD.Infrastructure/Data/MerengueRDApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MerengueRD.Domain.Entities;
+
+
+namespace MerengueRD.Infrastructure.Data
+{
+    public class MerengueRDApplicationContext : DbContext
+    {
+        public MerengueRDApplicationContext(DbContextOptions op) : base(op) {}
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<EventChronological> EventChronological { get; set; }
+        public DbSet<QuestionQuiz> questionQuizzes { get; set; }
+        public DbSet<Song> Song { get; set; }
+        public DbSet<QuizMusical> quizMusicals { get; set; } 
+
+        protected MerengueRDApplicationContext()
+        {
+        }
+    }
+}
